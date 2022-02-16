cff8991a2e1924ba56e9c91de51a1726027f9a85
LiamOsler <liam.osler@dal.ca>
2022-02-16 04:43:57 -0400

    implemented basic datatables sorting

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
-var express = require('express');
-var app = express();
+let express = require('express');
+
+let app = express();
 app.use('/jquery/', express.static(__dirname + '/node_modules/jquery'));
 app.use('/@popperjs/', express.static(__dirname + '/node_modules/@popperjs/core/dist/umd'));
 app.use('/bootstrap/css/', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/bootstrap/js/', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/datatables/js/', express.static(__dirname + '/node_modules/datatables/media/js'));
+app.use('/datatables/css/', express.static(__dirname + '/node_modules/datatables/media/css'));
+app.use('/datatables/images/', express.static(__dirname + '/node_modules/datatables/media/images'));
 app.use('/styles/', express.static(__dirname + '/styles'));
 app.set('view engine', 'ejs');
 
 let categoryData = fs.readFileSync('data/categories.json');
 let categories = JSON.parse(categoryData);
+
+let catalogData = fs.readFileSync('data/ryzen.json');
+let catalog = JSON.parse(catalogData);
+
+console.log(catalog.data[1]);
+
 
 // index page
 app.get('/', function(req, res) {
@@ -19,7 +29,27 @@ app.get('/', function(req, res) {
   });
 });
 
-// about page
+app.get('/catalog/', function(req, res) {
+  categories;
+  catalog;
+  res.render('pages/catalog',  {
+    categories: categories,
+    catalog: catalog
+  });
+});
+
+app.get('/catalog/:query', function (req, res){
+  categories;
+  catalog;
+  let searchQuery = req.params;
+  res.render('pages/catalog',  {
+    searchQuery: searchQuery,
+    categories : categories,
+    catalog: catalog
+  });
+});
+
+
 app.get('/about', function(req, res) {
   res.render('pages/about');
 });
